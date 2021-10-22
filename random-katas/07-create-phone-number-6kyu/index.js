62f0f9791b3c12db36ccbd55d36568232983119f
Dennis Kurochkin <dennis.kurochkin@gmail.com>
2021-10-22 22:39:42 +0300

    learn: add one more kata

---
+++ random-katas/07-create-phone-number-6kyu/index.js
@@ -0,0 +1,12 @@
+/**
+ * @see https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/javascript
+ */
+
+/**
+ * Solution 1
+ * Time Complexity: O(1)
+ * Space Complexity: O(1)
+ */
+function createPhoneNumber(nums) {
+  return `(${nums[0]}${nums[1]}${nums[2]}) ${nums[3]}${nums[4]}${nums[5]}-${nums[6]}${nums[7]}${nums[8]}${nums[9]}`
+}
